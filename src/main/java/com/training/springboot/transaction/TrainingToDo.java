dd48719ddf085fdebd1040a72c8dd4db66320ed3
akhmadreiza <reizaarmando@gmail.com>
2022-01-12 11:52:38 +0700

    init pre pert 2

---
+++ src/main/java/com/training/springboot/transaction/TrainingToDo.java
@@ -0,0 +1,19 @@
+package com.training.springboot.transaction;
+
+public class TrainingToDo {
+    //TODO: Pert 2
+    /*
+    *
+    * - Divide Project Package
+    * - Create Address DTO
+    * - Create AddressService class and impl
+    * - Create static address data
+    * - Create GET controller
+    * - Test GET controller
+    * - Create POST, PATCH controller
+    * - Test POST, PATCH controller
+    * - Create input Validation
+    * - Handling and throwing exception to client
+    *
+    * */
+}
