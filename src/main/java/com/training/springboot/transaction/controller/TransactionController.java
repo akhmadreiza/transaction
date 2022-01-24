a2dbbb7e692f4fec5ffa9a25f015ae2bab6efdce
akhmadreiza <reizaarmando@gmail.com>
2022-01-24 20:38:39 +0700

    update controller p5

--- src/main/java/com/training/springboot/transaction/controller/TransactionController.java
+++ src/main/java/com/training/springboot/transaction/controller/TransactionController.java
@@ -3,14 +3,12 @@ package com.training.springboot.transaction.controller;
 import com.training.springboot.transaction.dto.PaymentCreationDto;
 import com.training.springboot.transaction.dto.PaymentDto;
 import com.training.springboot.transaction.service.TransactionService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/transactions")
 public class TransactionController {
 
     private TransactionService transactionService;
