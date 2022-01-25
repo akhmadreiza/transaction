4dc87e4ddce45983d3f63db30f56bf9db7d70d76
akhmadreiza <reizaarmando@gmail.com>
2022-01-25 22:48:23 +0700

    update latest p5

--- src/main/java/com/training/springboot/transaction/repository/TransactionHistoryRepository.java
+++ src/main/java/com/training/springboot/transaction/repository/TransactionHistoryRepository.java
@@ -3,6 +3,8 @@ package com.training.springboot.transaction.repository;
 import com.training.springboot.transaction.entity.TransactionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
-    TransactionHistory findByTransactionId(String transactionId);
+    Optional<TransactionHistory> findByTransactionId(String transactionId);
 }
